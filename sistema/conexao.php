46ae3786ff43fc2241bc01d4645f7c14287e2e6a
Diogo Santos <97601965+diogosec@users.noreply.github.com>
2024-09-16 19:21:41 -0300

    modified:   index.php 	modified:   sistema/autenticar.php 	new file:   sistema/conexao.php 	new file:   sistema/img/fundomobile.jpg 	new file:   sistema/img/logobarb.ico 	modified:   sistema/index.php 	new file:   sistema/painel/index.php 	new file:   sistema/painel/logout.php 	new file:   src/base-do-projeto-mobile.png

---
+++ sistema/conexao.php
@@ -0,0 +1,19 @@
+<?php 
+
+$banco = 'barbearia';
+$usuario = 'root';
+$senha = '';
+$servidor = 'localhost';
+
+date_default_timezone_set('America/Sao_Paulo');
+
+try {
+	$pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", "$usuario", "$senha");
+} catch (Exception $e) {
+	echo 'Não conectado ao Banco de Dados! <br><br>' .$e;
+}
+
+$nome_sistema = 'Barber';
+$email_sistema = 'adm1@barber.com';
+
+ ?>
--- sistema/index.php
+++ sistema/index.php
@@ -1,9 +1,22 @@
+<?php 
+require_once("conexao.php");
+$senha = '123';
+$senhacrip = md5($senha);
+
+$query = $pdo->query("SELECT * from usuarios where nivel = 'Administrador'");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$total_reg = @count($res);
+if($total_reg == 0){
+	$pdo->query("INSERT INTO usuarios SET nome = 'Diogo Santos', email = '$email_sistema', cpf = '000.000.000-00', senha = '$senha', senhacrip = '$senhacrip', nivel = 'Administrador', data = curDate(), ativo = 'Sim'");
+}
+ ?>	
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Salão</title>
+	<title>Barber</title>
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
@@ -12,6 +25,7 @@
 	<script src="http://mymaplist.com/js/vendor/TweenLite.min.js"></script>
 	<!-- This is a very simple parallax effect achieved by simple CSS 3 multiple backgrounds, made by http://twitter.com/msurguy -->
 	<link rel="stylesheet" type="text/css" href="css/estilo-login.css">
+	<link rel="icon" type="image/png" href="img/logobarb.ico">
 </head>
 <body>
 
@@ -37,11 +51,11 @@
 			    		    <input class="form-control" placeholder="E-mail" name="email" type="text">
 			    		</div>
 			    		<div class="form-group">
-			    			<input class="form-control" placeholder="Senha" name="password" type="password" value="">
+			    			<input class="form-control" placeholder="Senha" name="senha" type="password" value="">
 			    		</div>
 			    		<div class="checkbox">
 			    	    	<label>
-			    	    		<input name="remember" type="checkbox" value="Remember Me"> Remember Me
+			    	    		<input name="remember" type="checkbox" value="Remember"> Lembre de mim
 			    	    	</label>
 			    	    </div>
 			    		<input class="btn btn-lg btn-primary btn-block" type="submit" value="Login">
